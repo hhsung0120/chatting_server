afe667bd17cead4941b46a511e6c686e3071469e
hshan <hhsung0120@naver.com>
2022-10-08 17:53:30 +0900

    Initial commit, UI 작업 새로 시작, 로직 확실히 이해 및 정리하기

---
+++ src/main/java/kr/heeseong/chatting_server/contorller/TestController.java
@@ -0,0 +1,4 @@
+package kr.heeseong.chatting_server.contorller;
+
+public class TestController {
+}
---
+++ src/test/java/kr/heeseong/chatting_server/ChattingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.heeseong.chatting_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
